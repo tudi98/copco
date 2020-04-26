a5ed78b67b3d177d4fecc83a5e58f6f6e453658d
Tudor Cozma <cozmatudor@gmail.com>
2020-04-26 20:53:17 +0300

    Remove code_generator package

--- cmd/gen.go
+++ cmd/gen.go
@@ -1,10 +1,14 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/spf13/cobra"
-	"github.com/tudi98/copco/code_generator"
+	"github.com/spf13/viper"
 	"io/ioutil"
 	"log"
+	"os"
+	"regexp"
+	"strings"
 )
 
 var genCmd = &cobra.Command{
@@ -25,9 +29,30 @@ func gen(file string) {
 	if err != nil {
 		log.Fatalf("Error while opening %s", file)
 	}
-	sourceCode, err := code_generator.GenerateSolution(string(content))
+	sourceCode, err := generateSourceCode(string(content))
 	err = ioutil.WriteFile(file, []byte(sourceCode), 0644)
 	if err != nil {
 		log.Fatalf("Error while writing to %s", file)
 	}
+}
+
+func generateSourceCode(sourceCode string) (string, error) {
+	r := regexp.MustCompile("#copco \".+\"")
+	lines := strings.Split(sourceCode, "\n")
+	sourceCode = ""
+	for _, line := range lines {
+		if r.MatchString(line) {
+			codePath := strings.Split(line, "\"")[1]
+			codePath = strings.ReplaceAll(codePath, "/", string(os.PathSeparator))
+			codePath = viper.GetString("COPCO_CUSTOM_CODE") + string(os.PathSeparator) + codePath
+			content, err := ioutil.ReadFile(codePath)
+			if err != nil {
+				return "", fmt.Errorf("could not find custom code file %s", codePath)
+			}
+			sourceCode += string(content) + "\n"
+		} else {
+			sourceCode += line + "\n"
+		}
+	}
+	return sourceCode, nil
 }
